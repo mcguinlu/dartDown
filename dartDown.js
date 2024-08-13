32c74c6e3b7383e446116428ec29f2c21b8de612
Luke McGuinness <luke.a.mcguiness@gmail.com>
2024-08-13 21:03:27 +0100

    Update to new CORS proxy

--- dartDown.js
+++ dartDown.js
@@ -1,82 +1,85 @@
 // Handle parameter changes
-const urlParams = window.location.search
-  .substring(1)
-  .split('&')
-  .reduce((params, urlParam) => {
-    let param = urlParam.split('=');
-    params[param[0]] = param[1];
-    return params;
-  }, {});
+const urlParams = new URLSearchParams(window.location.search);
 
 // number of trains to display in either direction
-const MAX_TRAINS_PER_DIRECTION = parseInt(urlParams.limit) || 3;
+const MAX_TRAINS_PER_DIRECTION = parseInt(urlParams.get('limit')) || 3;
 // station of origin abbreviation - https://api.bart.gov/docs/overview/abbrev.aspx
-const STATION_CODE = urlParams.station || 'gcdk';
+const STATION_CODE = urlParams.get('station') || 'gcdk';
 // departure times below the cutoff will not be displayed
-const MINUTE_CUTOFF = parseInt(urlParams.minute_cutoff) || 3;
+const MINUTE_CUTOFF = parseInt(urlParams.get('minute_cutoff')) || 3;
 // How often to poll for updates
-const UPDATE_MS = parseInt(urlParams.refresh)*1000 || 60000;
+const UPDATE_MS = parseInt(urlParams.get('refresh')) * 1000 || 180000;
 
 // Scale content
 const fontWidth = 100 / (MAX_TRAINS_PER_DIRECTION * 1.5);
 document.body.style.fontSize = `${fontWidth}vw`;
 
 async function dartDown() {
-
   console.log('dartDown');
 
-  const response = await fetch(
-    `https://api.factmaven.com/xml-to-json/?xml=http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode=${STATION_CODE}`,
-  );
+  var origin = window.location.protocol + '//' + window.location.host;
 
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      'https://corsproxy.io/?' +
+      `https://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode=${STATION_CODE}`
+    );
 
-  // Check if API has returned any trains
-  trains_available = data.hasOwnProperty('ArrayOfObjStationData');
+    const xmlText = await response.text();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
 
-  
-  const estimatesForStation = data.ArrayOfObjStationData.objStationData
 
-  const estimates = estimatesForStation
-    // Filter estimates that don't match criteria
-    .filter((estimate) => estimate.Duein >= MINUTE_CUTOFF) // Keep trains that are due in less than the cutoff
-    .filter((estimate) => estimate.Traintype == 'DART') // Keep only DART trains
-    // Transform 'Leaving' to 00 and ensure all times are double digits
-    .map((estimate) => {
-      estimate.Duein = estimate.Duein === 'Leaving' ? '00' : estimate.Duein;
-      estimate.Duein = estimate.Duein.length < 2 ? '0' + estimate.Duein : estimate.Duein;
-      return estimate;
-    })
-    // Sort departures from soonest to latest
-    .sort((a, b) => a.Duein - b.Duein);
 
-  // Hide the error state
-  document.getElementById('disconnected').style.display = 'none';
+    // Extracting data from XML
+    const estimatesForStation = Array.from(xmlDoc.querySelectorAll('objStationData')).map((estimate) => {
+      return Array.from(estimate.children).reduce((obj, child) => {
+        obj[child.nodeName] = child.textContent.trim();
+        return obj;
+      }, {});
+    });
 
-  // Remove existing estimates from DOM
-  Array.from(document.getElementsByClassName('estimate')).forEach((line) => {
-    line.remove();
-  });
+    // Filter and process estimates
+    const estimates = estimatesForStation
+      .filter((estimate) => parseInt(estimate.Duein) >= MINUTE_CUTOFF) // Keep trains that are due in less than the cutoff
+      .filter((estimate) => estimate.Traintype === 'DART') // Keep only DART trains
+      .map((estimate) => {
+        estimate.Duein = estimate.Duein === 'Leaving' ? '00' : estimate.Duein;
+        estimate.Duein = estimate.Duein.length < 2 ? '0' + estimate.Duein : estimate.Duein;
+        return estimate;
+      })
+      .sort((a, b) => parseInt(a.Duein) - parseInt(b.Duein));
 
-  let directionCount = {
-    northbound: 0,
-    southbound: 0,
-  };
+    // Hide the error state
+    document.getElementById('disconnected').style.display = 'none';
 
-  // Add the new estimates to the DOM
-  estimates.forEach((estimate) => {
-    const direction = estimate.Direction.toLowerCase();
-    if (directionCount[direction] < MAX_TRAINS_PER_DIRECTION) {
-      document
-        .getElementById(direction)
-        .insertAdjacentHTML(
-          'beforeEnd',
-          `<div class="estimate ${estimate.Destination.toLowerCase()}">${estimate.Duein}</div>`,
-        );
+    // Remove existing estimates from DOM
+    Array.from(document.getElementsByClassName('estimate')).forEach((line) => {
+      line.remove();
+    });
 
-      directionCount[direction]++;
-    }
-  });
+    let directionCount = {
+      northbound: 0,
+      southbound: 0,
+    };
+
+    // Add the new estimates to the DOM
+    estimates.forEach((estimate) => {
+      const direction = estimate.Direction.toLowerCase();
+      if (directionCount[direction] < MAX_TRAINS_PER_DIRECTION) {
+        document
+          .getElementById(direction)
+          .insertAdjacentHTML(
+            'beforeEnd',
+            `<div class="estimate ${estimate.Destination.toLowerCase()}">${estimate.Duein}</div>`
+          );
+
+        directionCount[direction]++;
+      }
+    });
+  } catch (error) {
+    displayErrorState(error);
+  }
 }
 
 // Display an icon on error
@@ -84,7 +87,7 @@ function displayErrorState(error) {
   console.log(error);
 
   // If API is showing no trains, say that
-  // Otherwise, show disonnected symbol on error
+  // Otherwise, show disconnected symbol on error
   if (trains_available) {
     document.getElementById('disconnected').style.display = 'flex';
   } else {
