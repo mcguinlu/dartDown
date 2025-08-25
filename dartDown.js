7e4cda8c077894d8513c07c1678e403acf4c63d1
Luke McGuinness <luke.a.mcguinness@gmail.com>
2025-08-25 19:30:47 +0100

    Add about section to pop-up, and add code to remove trains terminating at the same station they are currently at

--- dartDown.js
+++ dartDown.js
@@ -99,6 +99,7 @@ async function dartDown() {
     })
     .filter((estimate) => parseInt(estimate.Duein) >= MINUTE_CUTOFF) // Keep trains that are due in less than the cutoff
     .filter((estimate) => estimate.Traintype === 'DART10' || estimate.Traintype === 'DART') // Keep only DART trains
+    .filter((estimate) => estimate.Destination !== estimate.Stationfullname)
     .sort((a, b) => parseInt(a.Duein) - parseInt(b.Duein));
 
     // Hide the error state
